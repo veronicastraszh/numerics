d07c5cd9894749c83d413863a938dfaedf792cc4
Veronica Straszheim <veronica.straszh@gmail.com>
2017-02-14 22:52:36 -0500

    Updates

--- src/gauss_seidel.jl
+++ src/gauss_seidel.jl
@@ -4,14 +4,21 @@ function gauss_seidel(A, b)::Vector{Float64}
     width = size(A)[1]
     E = eye(width)
     x = zeros(width)
-    err = 1.0
-    while err > .00001
+    err = Inf
+    precision = 1.0e-6 * dot(b,b)
+    iteration = 0
+    while err > precision
+        iteration += 1
+        if iteration > 10000
+            throw("Max iterations")
+        end
         r = b - A*x
         dim=indmax(map(abs,r))
+        #dim = mod(iteration-1,width)+1
         v = E[:,dim]
         err = dot(x-x_star,A*(x-x_star))
         ##imp = dot(r,v)^2 / A[dim,dim]
-        println(err, " ", r[dim], " ", dot(r,r))
+        println(iteration, " ", err, " ", r[dim], " ", dot(r,r))
         α = dot(v,r)/dot(v,A*v)
         x = x + α * v
     end
